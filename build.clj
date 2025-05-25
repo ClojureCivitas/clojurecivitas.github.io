1292db23ff09d98f29b7b6f1e038fe2f7ac6db37
Timothy Pratley <timothypratley@gmail.com>
2025-05-24 23:33:00 -0700

    rename repo to publish as home

--- build.clj
+++ build.clj
@@ -2,7 +2,7 @@
   (:refer-clojure :exclude [test])
   (:require [clojure.tools.build.api :as b]))
 
-(def lib 'io.github.clojurecivitas/clojurecivitas)
+(def lib 'io.github.clojurecivitas/clojurecivitas.github.io)
 (def version "0.1.0-SNAPSHOT")
 (def main 'civitas.main)
 (def class-dir "target/classes")
