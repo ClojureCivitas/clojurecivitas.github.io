4a0f4e14fa69861ccb5005dac34e57dd7af6d7aa
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-09-09 21:53:21 +0100

    Add intermediate files

--- src/opengl_visualization/main.clj
+++ src/opengl_visualization/main.clj
@@ -1,16 +1,53 @@
 ^{:kindly/hide-code true
   :clay             {:title  "OpenGL Visualization with LWJGL"
-                     :external-requirements ["integration.txt"]
+                     :external-requirements ["Xorg"]
                      :quarto {:author   [:janwedekind]
                               :type     :post
-                              :date     "2025-09-08"
+                              :date     "2025-09-09"
                               :category :clojure
                               :tags     [:visualization]}}}
 (ns opengl-visualization.main
     (:require [clojure.java.io :as io])
-    (:import [javax.imageio ImageIO]))
+    (:import [javax.imageio ImageIO]
+             [org.lwjgl.glfw GLFW]
+             [org.lwjgl.opengl GL GL11]
+             [org.lwjgl.stb STBImageWrite]))
 
 ;; https://scicloj.github.io/clay/clay_book.examples.html
-;; clay -M:clay needs an editing change before it displays
+;;
+;; clj -M:clay -A:markdown
+;; quarto render site
 
-(slurp "integration.txt")
+(defn tmpname
+  []
+  (str (System/getProperty "java.io.tmpdir") "/civitas-" (java.util.UUID/randomUUID) ".tmp"))
+
+
+(GLFW/glfwInit)
+
+(def window-width 640)
+(def window-height 480)
+
+(def window
+  (do
+    (GLFW/glfwDefaultWindowHints)
+    (GLFW/glfwWindowHint GLFW/GLFW_DEPTH_BITS 24)
+    (GLFW/glfwWindowHint GLFW/GLFW_STENCIL_BITS 8)
+    (GLFW/glfwWindowHint GLFW/GLFW_VISIBLE GLFW/GLFW_FALSE)
+    (GLFW/glfwCreateWindow window-width window-height "Invisible Window" 0 0)))
+(do
+  (GLFW/glfwMakeContextCurrent window)
+  (GL/createCapabilities))
+
+(GL11/glClearColor 1.0 0.5 0.25 1.0)
+(GL11/glClearDepth 0.0)
+(GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT GL11/GL_DEPTH_BUFFER_BIT))
+
+(def buffer (java.nio.ByteBuffer/allocateDirect (* 4 window-width window-height)))
+(GL11/glReadPixels 0 0 window-width window-height GL11/GL_RGBA GL11/GL_UNSIGNED_BYTE buffer)
+(let [filename (tmpname)]
+  (STBImageWrite/stbi_write_png filename window-width window-height 4 buffer (* 4 window-width))
+  (-> filename io/file (ImageIO/read)))
+
+(GLFW/glfwDestroyWindow window)
+(GLFW/glfwTerminate)
