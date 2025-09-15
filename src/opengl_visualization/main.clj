0495271872f2e94952756df23e193051678bf46d
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-09-16 00:35:24 +0100

    Render a color ramp

--- src/opengl_visualization/main.clj
+++ src/opengl_visualization/main.clj
@@ -9,8 +9,9 @@
 (ns opengl-visualization.main
     (:require [clojure.java.io :as io])
     (:import [javax.imageio ImageIO]
+             [org.lwjgl BufferUtils]
              [org.lwjgl.glfw GLFW]
-             [org.lwjgl.opengl GL GL11 GL20]
+             [org.lwjgl.opengl GL GL11 GL15 GL20 GL30]
              [org.lwjgl.stb STBImageWrite]))
 
 ;; ### Getting dependencies
@@ -28,8 +29,9 @@
 ;;            'org.lwjgl/lwjgl-stb$natives-linux    {:mvn/version "3.3.6"}})
 ;; (require '[clojure.java.io :as io])
 ;; (import '[javax.imageio ImageIO]
+;;         '[org.lwjgl BufferUtils]
 ;;         '[org.lwjgl.glfw GLFW]
-;;         '[org.lwjgl.opengl GL GL11 GL20]
+;;         '[org.lwjgl.opengl GL GL11 GL15 GL20 GL30]
 ;;         '[org.lwjgl.stb STBImageWrite])
 ;; ```
 
@@ -39,12 +41,13 @@
 (def window-width 640)
 (def window-height 480)
 
-;; We use temporary files to convert STB images to ImageIO images.
+;; We define a function to create a temporary file name.
 (defn tmpname
   []
   (str (System/getProperty "java.io.tmpdir") "/civitas-" (java.util.UUID/randomUUID) ".tmp"))
 
-;; We define a function to take a screenshot of the window.
+;; The following function is used to create screenshots for this article.
+;; We read the pixels, write them to a temporary file using the STB library and then convert it to an ImageIO object.
 (defn screenshot
   []
   (let [filename (tmpname)
@@ -57,7 +60,7 @@
 (GLFW/glfwInit)
 
 ;; Now we create an invisible window.
-;; You can create a visisble window if you want to.
+;; You can create a visisble window if you want to by not setting the visibility hint to false.
 (def window
   (do
     (GLFW/glfwDefaultWindowHints)
@@ -65,6 +68,16 @@
     (GLFW/glfwWindowHint GLFW/GLFW_STENCIL_BITS 8)
     (GLFW/glfwWindowHint GLFW/GLFW_VISIBLE GLFW/GLFW_FALSE)
     (GLFW/glfwCreateWindow window-width window-height "Invisible Window" 0 0)))
+
+;; If you have a visible window, you can show it as follows.
+;; ```Clojure
+;; (GLFW/glfwShowWindow window)
+;; ```
+;;
+;; Note that if you are using a visible window, you always need to swap buffers after rendering.
+;; ```Clojure
+;; (GLFW/glfwSwapBuffers window)
+;; ```
 
 ;; Next we need to set up OpenGL rendering for this window.
 (do
@@ -80,9 +93,9 @@
   (GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT GL11/GL_DEPTH_BUFFER_BIT))
   (screenshot))
 
-;; ### Defining methods for creating shader programs
+;; ### Creating shader programs
 ;;
-;;
+;; We define a convenience function to compile a shader and handle any errors.
 (defn make-shader [source shader-type]
   (let [shader (GL20/glCreateShader shader-type)]
     (GL20/glShaderSource shader source)
@@ -91,6 +104,7 @@
       (throw (Exception. (GL20/glGetShaderInfoLog shader 1024))))
     shader))
 
+;; We also define a convenience function to link a program and handle any errors.
 (defn make-program [& shaders]
   (let [program (GL20/glCreateProgram)]
     (doseq [shader shaders]
@@ -101,9 +115,93 @@
       (throw (Exception. (GL20/glGetProgramInfoLog program 1024))))
     program))
 
+;; The following code shows a simple vertex shader which passes through the vertex coordinates.
+(def vertex-source "#version 130
+in mediump vec3 point;
+void main()
+{
+  gl_Position = vec4(point, 1);
+}")
+
+;; In the fragment shader we use the pixel coordinates to output a color ramp.
+;; The uniform variable iResolution will later be set to the window resolution.
+(def fragment-source "#version 130
+uniform vec2 iResolution;
+out vec4 fragColor;
+void main()
+{
+  fragColor = vec4(gl_FragCoord.xy / iResolution.xy, 0, 1);
+}")
+
+;; Let's compile the shaders and link the program.
+(def vertex-shader (make-shader vertex-source GL20/GL_VERTEX_SHADER))
+(def fragment-shader (make-shader fragment-source GL20/GL_FRAGMENT_SHADER))
+(def program (make-program vertex-shader fragment-shader))
+
+;; ### Creating vertex buffer data
+;;
+;; To provide the shader program with vertex data we are going to define just a single quad consisting of four vertices.
+;;
+;; First we define a macro and use it to define convenience functions for converting arrays to LWJGL buffer objects.
+(defmacro def-make-buffer [method create-buffer]
+  `(defn ~method [data#]
+     (let [buffer# (~create-buffer (count data#))]
+       (.put buffer# data#)
+       (.flip buffer#)
+       buffer#)))
+
+(def-make-buffer make-float-buffer BufferUtils/createFloatBuffer)
+(def-make-buffer make-int-buffer BufferUtils/createIntBuffer)
+
+;; We define a simple background quad spanning the entire window.
+;; We use normalised device coordinates (NDC) which are between -1 and 1.
+(def vertices
+  (float-array [ 1.0  1.0 0.0
+                -1.0  1.0 0.0
+                -1.0 -1.0 0.0
+                 1.0 -1.0 0.0]))
+
+;; The index array defines the order of the vertices.
+(def indices
+  (int-array [0 1 2 3]))
+
+;; ### Setting up the vertex buffer
+;;
+;; We define a vertex array object (VAO) which acts like a context for the vertex and index buffer.
+(def vao (GL30/glGenVertexArrays))
+(GL30/glBindVertexArray vao)
+
+;; We define a vertex buffer object (VBO) which contains the vertex data.
+(def vbo (GL15/glGenBuffers))
+(GL15/glBindBuffer GL15/GL_ARRAY_BUFFER vbo)
+(def vertices-buffer (make-float-buffer vertices))
+(GL15/glBufferData GL15/GL_ARRAY_BUFFER vertices-buffer GL15/GL_STATIC_DRAW)
+
+;; We also define an index buffer object (IBO) which contains the index data.
+(def idx (GL15/glGenBuffers))
+(GL15/glBindBuffer GL15/GL_ELEMENT_ARRAY_BUFFER idx)
+(def indices-buffer (make-int-buffer indices))
+(GL15/glBufferData GL15/GL_ELEMENT_ARRAY_BUFFER indices-buffer GL15/GL_STATIC_DRAW)
+
+;; The data of each vertex is defined by 3 floats (x, y, z).
+;; We need to specify the layout of the vertex buffer object so that OpenGL knows how to interpret it.
+(GL20/glVertexAttribPointer (GL20/glGetAttribLocation program "point") 3 GL11/GL_FLOAT false (* 3 Float/BYTES) (* 0 Float/BYTES))
+(GL20/glEnableVertexAttribArray 0)
+
+;; ### Rendering the quad
+;;
+;; We select the program and define the uniform variable iResolution.
+(GL20/glUseProgram program)
+(GL20/glUniform2f (GL20/glGetUniformLocation program "iResolution") window-width window-height)
+
+;; Since the correct VAO is already bound, we are now ready to draw the quad.
+(GL11/glDrawElements GL11/GL_QUADS 4 GL11/GL_UNSIGNED_INT 0)
+(screenshot)
 
 ;; ### Finishing up
-;;
+
+(GL20/glDeleteProgram program)
+
 ;; When we are finished, we destroy the window.
 (GLFW/glfwDestroyWindow window)
 
