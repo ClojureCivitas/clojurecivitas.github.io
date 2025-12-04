994431d1000433061bfe4bf54b92b815c241196a
kloimhardt <markus.agwin@gmx.net>
2025-12-04 17:44:28 +0100

    one

--- src/mentat_collective/emmy/appendix_scittlerepl.clj
+++ src/mentat_collective/emmy/appendix_scittlerepl.clj
@@ -1,18 +1,42 @@
 ^{:kindly/hide-code true
-  :clay             {:title  "scittle repl"
-                     :quarto {:type     :draft
-                              ;; :sidebar  "emmy-fdg"
+  :clay             {:title  "The Scittle Repl"
+                     :quarto {:type     :post
                               :date     "2025-12-04"
+                              :image    "GClef.svg"
                               :category :libs
-                              :tags     [:emmy :physics]}}}
+                              :tags     [:scittle :repl]}}}
 (ns mentat-collective.emmy.appendix-scittlerepl
-  (:refer-clojure :exclude [+ - * / zero? compare divide numerator
-                            denominator
-                            time infinite? abs ref partial =])
-  (:require [scicloj.kindly.v4.api :as kindly]
-            [scicloj.kindly.v4.kind :as kind]))
+  (:require
+   [sci.nrepl.browser-server :as nrepl]
+   [scicloj.kindly.v4.api :as kindly]
+   [scicloj.kindly.v4.kind :as kind]))
+
+(comment 1)
+
+;; ## Starting a ClojureScript Repl in Clay
+
+;; With this notebook running locally, uncomment the following line ...
+
+;; (reset! start-scittle-nrepl-server? true)
+
+;; ... and reload the notebook.
+
+;; Then open an arbitrary (not .clj but) .cljs ClojureScript file.
+
+;; In your editor, open a repl connection. For example in Emacs/Cider, this means the following 5 steps: (I) *(sesman-start)* (II) choose cider-connect-cljs (III) select localhost (IV) port 1339, followed by (V) the REPL type nbb.
+
+;; Remark: in my setup, all this does not work when I connect my editor to a normal Clojure Repl as well. Instead of using a Clojure-Repl, to reload the notebook I start Clay with the file-watcher. And then ...
+
+;; Voila, you have a nice ClojureScript Repl.
 
 ^:kindly/hide-code
+(kind/hiccup
+  [:img {:src "GClef.svg"}])
+
+;; ## The inner workings of Scittle and sci.nrepl
+
+;; The following code is just to reveal the inner workings, you should already be happily repling in your .cljs file.
+
 (kind/hiccup
   [:div
    [:script "var SCITTLE_NREPL_WEBSOCKET_PORT = 1340"]
@@ -22,14 +46,30 @@
    [:script {:src "https://cdn.jsdelivr.net/npm/scittle@0.6.22/dist/scittle.nrepl.js"}]
    ])
 
-;; make sure that, in your local directory, there exists the file [browser_server](https://clojurecivitas.github.io/mentat_collective/emmy/browser_server.html)
+(defonce start-scittle-nrepl-server? (atom false))
 
-;; Then in the terminal start babashka in the following way
+(defonce active-scittle-nrepl-server? (atom false))
+
+(when (and start-scittle-nrepl-server?
+           (not active-scittle-nrepl-server?))
+  (reset! active-scittle-nrepl-server? true)
+  (nrepl/start! {:nrepl-port 1339 :websocket-port 1340})
+  :end_when)
+
+;; ## Also with Babashka
+
+;; It is possible to start the repl server entirely without Java.
+
+;; Make sure that, in your local directory, there exists the file [browser_server.clj](https://github.com/ClojureCivitas/clojurecivitas.github.io/blob/main/src/mentat_collective/emmy/browser_server.clj)
+
+;; Then in the terminal, start babashka in the following way
+
 ^:kindly/hide-code
 (kind/code
   "bb -cp . -e \"(require '[browser-server :as nrepl]) (nrepl/start! {:nrepl-port 1339 :websocket-port 1340}) (deref (promise))\"")
 
+;; For completeness, also the Clojure CLI version
 
-;; then open a (not .clj but) .cljs file in your editor
-
-;; In Cider, choose cider-connect-cljs, select localhost, port 1339, followed by the REPL type nbb .
+^:kindly/hide-code
+(kind/code
+  "clj -Sdeps \"{:deps {io.github.babashka/sci.nrepl {:mvn/version \\\"0.0.2\\\"}}}\" -M -e \"(require '[sci.nrepl.browser-server :as nrepl]) (nrepl/start! {:nrepl-port 1339 :websocket-port 1340})\"")
