d633f3732ab6467b7b03936968f9b4c6c5055d71
kloimhardt <markus.agwin@gmx.net>
2025-12-05 17:15:04 +0100

    Add name to Scittle Repl post (#215)
    * small
    
    * better explanantion

--- src/mentat_collective/emmy/appendix_scittlerepl.clj
+++ src/mentat_collective/emmy/appendix_scittlerepl.clj
@@ -1,6 +1,7 @@
 ^{:kindly/hide-code true
   :clay             {:title  "The Scittle Repl"
-                     :quarto {:type     :post
+                     :quarto {:author   :kloimhardt
+                              :type     :post
                               :date     "2025-12-04"
                               :image    "appendix_scittlerepl.png"
                               :category :libs
@@ -15,17 +16,21 @@
 
 ;; ## Starting the Scittle Repl of this Clay Notebook
 
-;; With this notebook running locally, and seeing this text in the browser window (i.e. notebook is loaded once), uncomment the following line ...
+(defonce start-scittle-nrepl-server
+  (memoize
+    (fn [start?]
+      (println "starting Scittle nrepl server")
+      (nrepl/start! {:nrepl-port 1339 :websocket-port 1340}))))
 
-;; (reset! start-scittle-nrepl-server? true)
+;; To start the nrepl server, in the local .clj file of this very notebook, uncomment the following line ...
 
-;; ... and reload the notebook.
+;; (start-scittle-nrepl-server true)
+
+;; ... and (re)load this notebook within your local Clay instance.
 
 ;; Then open an arbitrary (not .clj but) .cljs ClojureScript file.
 
-;; In your editor, open a repl connection. For example in Emacs/Cider, this means the following 5 steps: (I) *(sesman-start)* (II) choose cider-connect-cljs (III) select localhost (IV) port 1339, followed by (V) the REPL type nbb.
-
-;; Remark: in my setup, to make this work, there must not be another Clojure Repl connection. Because several connections seem to confuse each other, instead of using a Clojure/JVM-Repl, to reload the notebook I start Clay with the file-watcher. With the file-watcher, I make sure that I save any .cljs file before changing this very .clj file of this notebook. An unsaved .cljs file keeps this .clj file from reloading on change. If you keep things tidy, then ...
+;; In your local editor, open a Repl connection. For example in Emacs/Cider, this means the following 5 steps: (I) *(sesman-start)* (II) choose cider-connect-cljs (III) select localhost (IV) port 1339, followed by (V) the REPL type nbb.
 
 ;; Voila, you have a nice Scittle Repl.
 
@@ -33,9 +38,28 @@
 (kind/hiccup
   [:img {:src "GClef.svg"}])
 
+
+;; ## The Scittle State
+
+;; The executive summary is that I can only want you to open Clay in file-watcher mode and observe the following
+
+^:kindly/hide-code
+(kind/table
+  {"Action"              ["Reload browser" "Save this very notebook .clj file" "Save any .cljs file"]
+   "Clean Scittle state" ["yes" "yes" "no"]})
+
+
+;; The Scittle Repl looses its state when the browser window is reloaded. This also means that the Scittle Repl looses its state when this very notebook .clj file is reloaded. As opposed to that, the Clojure/JVM state persists in any case. In fact, this very notebook, in using defonce, relies on a persistent Clojure/JVM state. But the possibility to produce a clean Scittle slate via browser reload can be of great advantage especially for the (domain-expert but) Repl-novice.
+
+;; This notebook works best when Clay is started in file-watcher mode via `clojure -M:clay`. It is not a good idea to open, next to the Scittle Repl, also a Clojure/JVM Repl connection. At least in my setup, opening more than one Repl connection leads to unstable behaviour. Act at your own discretion.
+
+;; With Clay's file watcher active, not only is this very notebook .clj file reloaded on change, but indeed also any Scittle .cljs file that is changed and saved is automatically reloaded. But as opposed to slate-cleaning browser-reload, saving a .cljs file keeps the Scittle state.
+
+;; In sum, while saving a .cljs file keeps the Scittle state, saving this very notebook .clj file cleans the Scittle state. But as I can only want you to happily repl in .cljs files exclusively, further changing and loading of this very notebook is not necessary. Of course, you are free to change and save this very notebook .clj file as often as you like, but make sure all your .cljs files are saved before you do so. Any unsaved Scittle .cljs file keeps this very notebook from reloading on change.
+
 ;; ## The inner workings of Scittle and sci.nrepl
 
-;; The following code is just to reveal the inner workings, you should already be happily repling in your .cljs file.
+;; The following code is just to reveal the inner workings, you should already be able to happily repl in your .cljs file.
 
 ;; While this Scittle Repl works perfect with the standard Scittle distribution, scittle-kitchen poses problems with my setup.
 
@@ -48,19 +72,9 @@
    [:script {:src "https://cdn.jsdelivr.net/npm/scittle@0.6.22/dist/scittle.nrepl.js"}]
    ])
 
-(defonce start-scittle-nrepl-server? (atom false))
-
-(defonce active-scittle-nrepl-server? (atom false))
-
-(when (and @start-scittle-nrepl-server?
-           (not @active-scittle-nrepl-server?))
-  (reset! active-scittle-nrepl-server? true)
-  (nrepl/start! {:nrepl-port 1339 :websocket-port 1340})
-  :end_when)
-
 ;; ## Also with Babashka
 
-;; It is possible to start the repl server entirely without Java.
+;; It is possible to start the nrepl server entirely without Java.
 
 ;; Make sure that, in your local directory, there exists the file [browser_server.clj](https://github.com/ClojureCivitas/clojurecivitas.github.io/blob/main/src/mentat_collective/emmy/browser_server.clj)
 
@@ -70,7 +84,7 @@
 (kind/code
   "bb -cp . -e \"(require '[browser-server :as nrepl]) (nrepl/start! {:nrepl-port 1339 :websocket-port 1340}) (deref (promise))\"")
 
-;; For completeness, also the Clojure CLI version
+;; For completeness, I also provide the Clojure CLI version
 
 ^:kindly/hide-code
 (kind/code
