a07dd8b9a1e3ce7acd5dc8803a438fbe7d849af8
daslu <daniel.slutsky@gmail.com>
2025-12-06 23:24:22 +0200

    dtype-next based geotiff processing - a draft

--- src/gis/geotiff.clj
+++ src/gis/geotiff.clj
@@ -16,7 +16,8 @@
    [tech.v3.tensor.dimensions :as dims]
    [tech.v3.libs.buffered-image :as bufimg]
    [tablecloth.api :as tc]
-   [clojure.java.io :as io]))
+   [clojure.java.io :as io]
+   [tech.v3.dataset.tensor :as ds-tensor]))
 ;; # Cloud Optimized GeoTIFFs in JVM Clojure 
 ;; ## Motivation
 ;; This document shows several different methods for handling COGs in JVM Clojure without
@@ -80,6 +81,8 @@
         {:images images
          :reader-format (.getFormatName reader)}))))
 
+(require '[tech.v3.datatype :as dtype])
+
 (defn get-pixel [^BufferedImage image x y]
   (let [raster (.getRaster image)
         ;; Handle images with an arbitrary number of bands.
@@ -135,17 +138,19 @@
 ;; For our purposes we can just add the tablecloth dependency and that will transitively
 ;; bring along the rest of the stack.
 
-(require '[tablecloth.api :as tc])
-
-(repeatedly 5 #(double-array 2 3))
-
+(require '[tablecloth.api :as tc]
+         '[tech.v3.tensor :as tensor]
+         '[tech.v3.dataset.tensor :as ds-tensor])
 
 
 (defn buffered-image->dataset [^BufferedImage img]
   (let [width (.getWidth img)
         height (.getHeight img)
         num-bands (.getNumBands (.getRaster img))
-        
+
+        ;; The following will be easier after dtype-next's
+        ;; [#133](https://github.com/cnuernber/dtype-next/issues/133)
+        ;; is resolved.
         tensor (-> img
                    dtype/->array-buffer
                    ;; height x width x bands
@@ -162,13 +167,22 @@
                                    (fn [y x] y)))
 
         ;; Build dataset
-        ds-map (into {:x xs :y ys}
-                     (for [i (range num-bands)]
-                       [(keyword (str "band-" i))
-                        (dtype/as-reader
-                         (tensor i))]))]
-    (tc/dataset ds-map)))
-
+        ds-map   (into {:x xs :y ys}
+                       (for [i (range num-bands)]
+                         [(keyword (str "band-" i))
+                          (dtype/as-reader
+                           (tensor i))]))]
+    (-> tensor
+        (tensor/reshape [(* height width)
+                         num-bands])
+        ds-tensor/tensor->dataset
+        (tc/add-columns {:x xs
+                         :y ys})
+        (tc/select-columns (concat [:x :y]
+                                   (range num-bands)))
+        (tc/rename-columns (into {} 
+                                 (for [i (range num-bands)]
+                                   [i (keyword (str "band-" i))]))))))
 
 (defonce ds (-> example-geotiff-medium
                 :images
@@ -200,7 +214,6 @@
 (defonce ds-with-approx-dvi (add-dvi ds))
 
 (require '[tech.v3.dataset :as ds]
-         '[tech.v3.datatype :as dtype]
          '[tech.v3.datatype.statistics :as dstats])
 (import '[java.awt Color])
 
@@ -223,35 +236,22 @@
   (add-approx-dvi-display ds-with-approx-dvi))
 
 (defn dataset->image
-  [dataset {:keys [r g b]}]
-  (let [xs (ds/column dataset :x)
-        ys (ds/column dataset :y)
-        rs (ds/column dataset r)
-        gs (ds/column dataset g)
-        bs (ds/column dataset b)
-
-        max-x (-> xs last int)
-        min-x (-> xs first int)
-        max-y (-> ys last int)
-        min-y (-> ys first int)
-
-        width (inc (- max-x min-x))
-        height (inc (- max-y min-y))
-
-        ;; Create the image
-        img (BufferedImage. width height BufferedImage/TYPE_INT_RGB)]
-
-    ;; Set pixels
-    (dotimes [i (ds/row-count dataset)]
-      (let [x (- (int (dtype/get-value xs i)) min-x)
-            y (- (int (dtype/get-value ys i)) min-y)
-            r (int (dtype/get-value rs i))
-            g (int (dtype/get-value gs i))
-            b (int (dtype/get-value bs i))
-            color (Color. r g b)]
-        (.setRGB img x y (.getRGB color))))
-
-    img))
+  [dataset {:as columns-mapping
+            :keys [r g b]}]
+  (let [height (->> dataset
+                    :y
+                    ((juxt dstats/max dstats/min))
+                    (apply -))
+        width (->> dataset
+                   :x
+                   ((juxt dstats/max dstats/min))
+                   (apply -))]
+    (-> columns-mapping
+        (update-vals dataset)
+        tc/dataset
+        ds-tensor/dataset->tensor
+        (tensor/reshape [height width 3])
+        bufimg/tensor->image)))
 
 (dataset->image ds-with-dvi-display
                 {:r :dvi-display
