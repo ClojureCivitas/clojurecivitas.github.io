865b8e3faaf3c3c95919d195995176d03bbb332a
Jan Wedekind (Dr) <jan@wedesoft.de>
2026-01-13 20:46:37 +0000

    testing out tensors

--- src/volumetric_clouds/main.clj
+++ src/volumetric_clouds/main.clj
@@ -9,16 +9,23 @@
                               :category :clojure
                               :tags     [:visualization]}}}
 (ns volumetric-clouds.main
-    (:require [clojure.java.io :as io]
-              [tech.v3.tensor :as dtt])
+    (:require [scicloj.kindly.v4.kind :as kind]
+              [clojure.java.io :as io]
+              [fastmath.vector :as fm]
+              [tech.v3.datatype :as dtype]
+              [tech.v3.tensor :as tensor])
     (:import [javax.imageio ImageIO]
              [org.lwjgl.opengl GL11]
              [org.lwjgl.stb STBImageWrite]
              ))
 
+;; https://clojurecivitas.github.io/dtype_next/image_processing_with_tensors.html
+
 ;; Hello
 (def window-width 640)
 (def window-height 480)
+
+
 
 (defn tmpdir
   []
@@ -37,3 +44,11 @@
     (STBImageWrite/stbi_write_png filename window-width window-height 4
                                   buffer (* 4 window-width))
     (-> filename io/file (ImageIO/read))))
+
+(def size 16)
+(def divisions 4)
+(def cellsize (/ size divisions))
+
+;; (def a (tensor/->tensor [[(fm/vec3 1 2 3) (fm/vec3 4 5 6)] [(fm/vec3 7 8 9) (fm/vec3 10 11 12)]]))
+
+(def a (tensor/compute-tensor [divisions divisions] (fn [y x] (apply fm/vec2 (repeatedly 2 #(rand cellsize))))))
