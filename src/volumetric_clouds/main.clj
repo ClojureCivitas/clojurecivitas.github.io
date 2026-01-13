eaebafbd4736a5dc35855f3ab55348e6c7c4e4a2
Jan Wedekind (Dr) <jan@wedesoft.de>
2026-01-13 22:49:45 +0000

    Created worley noise

--- src/volumetric_clouds/main.clj
+++ src/volumetric_clouds/main.clj
@@ -8,16 +8,18 @@
                               :date     "2026-01-13"
                               :category :clojure
                               :tags     [:visualization]}}}
+
 (ns volumetric-clouds.main
     (:require [scicloj.kindly.v4.kind :as kind]
               [clojure.java.io :as io]
               [fastmath.vector :as fm]
               [tech.v3.datatype :as dtype]
-              [tech.v3.tensor :as tensor])
+              [tech.v3.tensor :as tensor]
+              [tech.v3.datatype.functional :as dfn]
+              [tech.v3.libs.buffered-image :as bufimg])
     (:import [javax.imageio ImageIO]
              [org.lwjgl.opengl GL11]
-             [org.lwjgl.stb STBImageWrite]
-             ))
+             [org.lwjgl.stb STBImageWrite]))
 
 ;; https://clojurecivitas.github.io/dtype_next/image_processing_with_tensors.html
 
@@ -26,29 +28,36 @@
 (def window-height 480)
 
 
-
-(defn tmpdir
-  []
-  (System/getProperty "java.io.tmpdir"))
-
-(defn tmpname
-  []
-  (str (tmpdir) "/civitas-" (java.util.UUID/randomUUID) ".tmp"))
-
-(defn screenshot
-  []
-  (let [filename (tmpname)
-        buffer   (java.nio.ByteBuffer/allocateDirect (* 4 window-width window-height))]
-    (GL11/glReadPixels 0 0 window-width window-height
-                       GL11/GL_RGBA GL11/GL_UNSIGNED_BYTE buffer)
-    (STBImageWrite/stbi_write_png filename window-width window-height 4
-                                  buffer (* 4 window-width))
-    (-> filename io/file (ImageIO/read))))
-
-(def size 16)
-(def divisions 4)
+(def size 512)
+(def divisions 32)
 (def cellsize (/ size divisions))
 
-;; (def a (tensor/->tensor [[(fm/vec3 1 2 3) (fm/vec3 4 5 6)] [(fm/vec3 7 8 9) (fm/vec3 10 11 12)]]))
+(def t (tensor/->tensor [[(fm/vec3 1 2 3) (fm/vec3 4 5 6)] [(fm/vec3 7 8 9) (fm/vec3 10 11 12)]]))
+t
 
-(def a (tensor/compute-tensor [divisions divisions] (fn [y x] (apply fm/vec2 (repeatedly 2 #(rand cellsize))))))
+(defn pt [y x] (fm/add (fm/mult (fm/vec2 x y) cellsize) (fm/vec2 (rand cellsize) (rand cellsize)) ))
+
+(defn clip [x] (if (>= x (/ size 2)) (- x size) (if (< x (/ size -2)) (+ x size) x)))
+
+(defn dist [a b] (fm/mag (fm/vec2 (clip (- (a 0) (b 0))) (clip (- (a 1) (b 1))))))
+
+(def random-points (tensor/clone (tensor/compute-tensor [divisions divisions] pt)))
+random-points
+
+(def worley
+  (tensor/clone
+    (tensor/compute-tensor [size size]
+                           (fn [y x]
+                               (let [center (fm/add (fm/vec2 x y) (fm/vec2 0.5 0.5))
+                                     divx   (quot x cellsize)
+                                     divy   (quot y cellsize)]
+                                 (apply min
+                                        (for [dy [-1 0 1] dx [-1 0 1]]
+                                             (dist (random-points (mod (+ divy dy) divisions) (mod (+ divx dx) divisions)) center)))))
+                           :double)))
+worley
+
+(def img (dfn/* (/ 255 (- (dfn/reduce-max worley) (dfn/reduce-min worley))) (dfn/- (dfn/reduce-max worley) worley)))
+img
+
+(bufimg/tensor->image img)
