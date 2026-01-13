b2f84384cadfa465a00a8f34edf3a117cec34db8
Timothy Pratley <timothypratley@gmail.com>
2026-01-12 21:47:14 -0800

    change column_combinations for test rebuild

--- src/data_visualization/aog/column_combinations.clj
+++ src/data_visualization/aog/column_combinations.clj
@@ -7,7 +7,7 @@
                   :description "Can we plot interesting charts for all columns of a dataset?"
                   :category :data-visualization
                   :tags [:datavis :clay]
-                  :keywords [:datavis]}}}
+                  :keywords [:datavis :data :summaries]}}}
 (ns data-visualization.aog.column-combinations
   (:require
    [clojure.string :as str]
