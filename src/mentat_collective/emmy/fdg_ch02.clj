0ad1a33a500f278edcc7d51d9a6abdc6d9508164
kloimhardt <markus.agwin@gmx.net>
2026-01-30 18:44:53 +0100

    Ch03 fnish

--- src/mentat_collective/emmy/fdg_ch02.clj
+++ src/mentat_collective/emmy/fdg_ch02.clj
@@ -130,7 +130,8 @@
 (define R2-polar (coordinate-system-at R2 :polar-cylindrical :origin))
 
 (define R2-rect-chi (chart R2-rect))
-(define R2-rect-chi-inverse (point R2-rect)) (define R2-polar-chi (chart R2-polar))
+(define R2-rect-chi-inverse (point R2-rect))
+(define R2-polar-chi (chart R2-polar))
 (define R2-polar-chi-inverse (point R2-polar))
 
 (print-expression
