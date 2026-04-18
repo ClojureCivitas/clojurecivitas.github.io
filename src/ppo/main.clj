f30aeb45aefaa3b8d9d341a4c14d4a77c23e8f27
Jan Wedekind (Dr) <jan@wedesoft.de>
2026-04-18 23:46:19 +0100

    Add CPU version of Torch to Github workflow

---
+++ src/ppo/main.clj
@@ -0,0 +1,13 @@
+^{:kindly/hide-code true
+  :clay             {:title  "Proximal Policy Optimization with Clojure and Pytorch"
+                     :external-requirements []
+                     :quarto {:author   [:janwedekind]
+                              :draft    true
+                              :description "A Clojure port of Jinghao's PPO implementation using Pytorch and Quil"
+                              :image    "pendulum.png"
+                              :type     :post
+                              :date     "2026-04-18"
+                              :category :ml
+                              :tags     [:physics :machine-learning :optimization :ppo :control]}}}
+
+(ns ppo.main)
