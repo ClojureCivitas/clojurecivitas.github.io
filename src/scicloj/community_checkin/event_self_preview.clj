d551297674a9402541eea68b0a377d28cf5e7ade
Siyoung Byun <syungb@gmail.com>
2026-04-24 18:06:36 +0900

    add a post for clojure community check-in event

---
+++ src/scicloj/community_checkin/event_self_preview.clj
@@ -0,0 +1,49 @@
+^{:kindly/hide-code true
+  :clay
+  {:title  "Clojure community check-in self preview"
+   :quarto {:author      :syungb
+            :description "A self preview before the Clojure Community Check-in event"
+            :image       "community-checkin-logo.png"
+            :type        :post
+            :date        "2026-04-24"
+            :category    :event
+            :tags        [:community-checkin]}}}
+(ns scicloj.community-checkin.event-self-preview)
+
+;; This weekend, Scicloj will host an online event called [Clojure community check-in](https://clojureverse.org/t/clojure-community-check-in/14864),
+;; and earlier we published [a short video](https://www.youtube.com/watch?v=dkxwZ9VGLCo)
+;; to make an introduction what it’s about.
+;; Then I have been thinking about answering myself those questions brought up
+;; in the video. Even though it’s very raw, and nothing much affirmative,
+;; I am going to see if I could share some of my answers in this post before the event.
+;;
+;;
+;; ## initiatives for this event
+;;
+;; Personally, I could think of a couple of reasons of why having this kind of event is helpful for the community.
+;;
+;; First, I think that we(as an individual in the tech industry) have been experiencing the fastest-paced changes in the industry’s history.  I naively thought that I was used to the fast paced industry and environment, but at the moment I am struggling to keep up with the current pace. I don’t think that it’s simply due to getting left behind. I think it’s due to overall gradually loosing human centric aspects in the workplaces. I am not convinced by a lot of recent hype around certain new technologies. And I am not ready to simply accept what’s happening, and affecting all of us, then move on.
+;;
+;; Second, personally, I have been quite concerned and depressed about what’s happening in globally and locally in many aspects. And I don’t want to feel bad about FOMO anymore in the industry. But sometimes I can’t help but feel insecure or second-guessing myself. So I think having a space to discuss these concerns is really valuable. There’s a saying I really like in Korean(directly translated, and I bet there are similar ones in other countries): “if you share some sadness with someone, it becomes a half, and if you share some happiness with someone, it becomes double”.
+;;
+;; Gathering and sharing stories itself could be very empowering, and I know we wouldn’t have answers or resolutions immediately, but I think our brains would be initiated to think further, and find ways to have our mind at ease.
+;;
+;;
+;; ## Q1. How’s my work-life in general
+;;
+;; My work was recently slowed down, it seemed my latest work was affected by global economic downturn unfortunately. I feel my job security has been thinning at this point, and it seems this applies to some of my old coworkers from Canada and the States, so there’s some anxiety. But I am grateful that recently I was able to start some conversations to potentially building something new.
+;;
+;; Personally, I have been doing okay, but maybe not, I have been constantly worried about my dear friends who are currently living in places in war, thinking of them, wishing them well, and feeling stressed of getting up every morning, and being afraid of seeing any sort of terrible news stories. My usually general greeting messages have been changed from “hope you are well!”, or “hope you’re having a great day!” to “hope you’re staying safe”.
+;;
+;;
+;; ## Q2. Clojure and me
+;;
+;;With my daily up-and-down moments, Clojure has been staying strong with me. The language has bee giving me opportunities to think more of the fundamentals of programming, and I have been looking at those implementations of built-in functions, rather than looking at hot techs, and libraries. My current “back 2 basics” mode has been quite good with Clojure. And I feel even though the overall community has been quieter, I still have noticed  many firm and persistent contributions and conversations on a daily basis, and these kinds of stability have been very helpful to easing my anxiety.
+;;
+;; Especially, several inspiring contributors in Scicloj have been continuously putting their effort to improve the libraries and sharing their projects stories in Clojure Civitas. Having something nice and exciting to read has been helping me to get me out of the random side tracks.
+;;
+;;
+;; ## Conclusion?
+;;
+;; I don’t have a conclusion, but I hope this short post would help some of you to start answering your own, hope to get to hear those, and get to know participants better during the event. Also I hope not only this time, but we could check-in each other more times further. I look forward to the event!
+;;
